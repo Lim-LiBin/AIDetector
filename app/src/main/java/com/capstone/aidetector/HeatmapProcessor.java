115f3cce02182fd0e98a87bad24d004017fe096e
Lim Li Bin <dst703410@gmail.com>
2026-03-07 18:45:02 +0900

    feat: 히트맵 시각화 로직

---
+++ app/src/main/java/com/capstone/aidetector/HeatmapProcessor.java
@@ -0,0 +1,33 @@
+package com.capstone.aidetector;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
+public class HeatmapProcessor {
+    //원본 이미지 및 전처리 규격과 동일하게 설정
+    private static final int TARGET_SIZE = 224;
+    private static final int MATRIX_SIZE = 7;
+
+    //7x7 행렬을 컬러 비트맵으로 변환하는 함수
+    public Bitmap createHeatmapImage(float[][] matrix) {
+        //7x7 크기의 비트맵 생성
+        Bitmap smallBitmap = Bitmap.createBitmap(MATRIX_SIZE, MATRIX_SIZE, Bitmap.Config.ARGB_8888);
+
+        for (int y = 0; y < MATRIX_SIZE; y++) {
+            for (int x = 0; x < MATRIX_SIZE; x++) {
+                float value = matrix[y][x]; //0.0~1.0 사이의 값
+
+                //수치에 맞는 색상 추출 로직
+                int alpha = (int) (value * 200);
+                int color = Color.argb(alpha, 255, 0, 0);
+
+                smallBitmap.setPixel(x, y, color);
+            }
+        }
+
+        //7x7 데이터를 224x224 크기로 키우기
+        Bitmap finalBitmap = Bitmap.createScaledBitmap(smallBitmap, TARGET_SIZE, TARGET_SIZE, true);
+
+        return finalBitmap;
+    }
+}
