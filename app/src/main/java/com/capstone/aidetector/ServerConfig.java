6d68e24ff41a79c2fc271a4fc4ea321d196671bb
hyunho0811 <2171119@hansung.ac.kr>
2026-05-09 15:52:09 +0900

    5/9 15:50 수정(서버 수동으로 다시)

--- app/src/main/java/com/capstone/aidetector/ServerConfig.java
+++ app/src/main/java/com/capstone/aidetector/ServerConfig.java
@@ -4,8 +4,11 @@ public class ServerConfig {
     // 개발 환경 (에뮬레이터로 테스트 시) - 서버, 앱 같은 기기
     //private static final String SERVER_URL = "http://10.0.2.2:5000/";
 
-    // 실제 기기 (같은 Wi-Fi 연결) - 서버, 앱 다른 기기
-    private static final String SERVER_URL = "http://3.107.250.218:5000/";
+
+    private static final String SERVER_URL = "https://subphrenic-intensionally-ardis.ngrok-free.dev/";
+    // AWS 서버
+    //private static final String SERVER_URL = "http://15.135.159.28:5000/";
+
     public static String getBaseUrl() {
         return SERVER_URL;
     }
